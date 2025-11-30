e79ff8ce8fc3d9c77b5bd14af6386b77110ba55b
SlyCright <sly.cright@gmail.com>
2025-11-30 10:23:23 +0300

    Pure Spring-Gradle setup

--- src/main/java/site/klade/web_app/KladeWebApplication.java
+++ src/main/java/site/klade/web_app/KladeWebApplication.java
@@ -1,4 +1,4 @@
-package site.klade.webapp;
+package site.klade.web_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +11,3 @@ public class KladeWebApplication {
 	}
 
 }
-
-// This comment is for triggering CI/CD pipeline
--- src/test/java/site/klade/web_app/KladeWebApplicationTests.java
+++ src/test/java/site/klade/web_app/KladeWebApplicationTests.java
@@ -1,4 +1,4 @@
-package site.klade.webapp;
+package site.klade.web_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

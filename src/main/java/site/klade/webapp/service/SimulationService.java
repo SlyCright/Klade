1c24be8579d33c2e44121a54a3b775b6c694d1a5
SlyCright <sly.cright@gmail.com>
2026-03-07 22:00:56 +0300

    refactor: simplify simulation service architecture and add new simulation class

--- src/main/java/site/klade/webapp/service/SimulationService.java
+++ src/main/java/site/klade/webapp/service/SimulationService.java
@@ -1,91 +1,40 @@
 package site.klade.webapp.service;
 
-import lombok.SneakyThrows;
-import org.springframework.scheduling.annotation.Async;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
-import site.klade.simulation.Simulation;
-import site.klade.simulation.SimulationDto;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.reflections.Reflections.log;
+import site.klade.simulation.SimulationSnapshotDto;
+import site.klade.webapp.simulation.Simulation;
 
 @Service
-@SuppressWarnings("unused")
 public class SimulationService {
 
-    private final AtomicReference<Simulation> currentSimulation = new AtomicReference<>();
-
-    private final AtomicBoolean running = new AtomicBoolean(false);
-
-    private Thread simulationThread;
-
-    @Async
-    @SneakyThrows
-    @SuppressWarnings("BusyWait")
-    public void startSimulation() {
-        log.info("Starting simulation");
-        simulationThread = Thread.currentThread();
-        running.set(true);
-        currentSimulation.set(new Simulation());
-        long loopCounter = 0;
-        while (running.get()) {
-            currentSimulation.get().update();
-            Thread.sleep(2);
-            if (loopCounter % 1000 == 0) {
-                SimulationDto simulationDto = currentSimulation.get().getSimulationDto();
-                log.info("Total ticks: {}, Rhyme node state changes: {}, Rhyme node active: {}",
-                        simulationDto.getTotalTicks(),
-                        simulationDto.getRhymeNodeStateChanges(),
-                        simulationDto.getIsRhymeNodeCurrentlyActive());
-            }
-            loopCounter++;
-            if (loopCounter > 1_000_000) stopSimulation();
-        }
-    }
-
-    @SneakyThrows
-    public void stopSimulation() {
-        running.set(false);
-        Thread.sleep(50);
-        if (simulationThread.isAlive()) {
-            simulationThread.interrupt();
-        }
-    }
-
-    @SneakyThrows
-    public void restartSimulation() {
-        stopSimulation();
-        Thread.sleep(50);
-        startSimulation();
-    }
-
-    public SimulationDto getSimulationDto() {
-        return currentSimulation.get().getSimulationDto();
-    }
-
-    public boolean isRunning() {
-        return running.get();
-    }
+    private final Simulation simulation = Simulation.withDefaultSettings();
 
     public void start() {
+        simulation.start();
+    }
+
+    public void runCertainGenerations(int number) {
+        simulation.runCertainGenerations(number);
 
     }
 
-    public void runCertainGenerations(int i) {
-
-    }
-
-    public void stop() {
-
+    public void stop() {simulation.stop();
     }
 
     public void reset() {
-
+        simulation.reset();
     }
 
-    public Object getSimulationSnapshot() {
-        return null;
+    @PreDestroy
+    public void shutdown() {
+        simulation.shutdown();
+    }
+
+    // TODO: use Spring's events instead of polling for data. Simulation should notify Spring
+    //  application when next generation is ready. Frontend should get notifications as well
+    public SimulationSnapshotDto getSimulationSnapshot() {
+        return simulation.getSnapshot();
     }
 }
---
+++ src/main/java/site/klade/webapp/simulation/Simulation.java
@@ -0,0 +1,33 @@
+package site.klade.webapp.simulation;
+
+import site.klade.simulation.SimulationSnapshotDto;
+
+public class Simulation {
+
+    public static Simulation withDefaultSettings() {
+        return null;
+    }
+
+    public void start() {
+    }
+
+    public void runCertainGenerations(int number) {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public void shutdown() {
+
+    }
+
+    public SimulationSnapshotDto getSnapshot() {
+        return null;
+    }
+}

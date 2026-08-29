5572fe4b9214cccb299379bd46a0ae8ee6658172
SlyCright <sly.cright@gmail.com>
2026-08-29 19:07:54 +0300

    Add GenomesDto for genome collection responses

---
+++ src/main/java/site/klade/webapp/controller/GenomesDto.java
@@ -0,0 +1,23 @@
+package site.klade.webapp.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+public class GenomesDto {
+
+    private List<GenomeDto> genomes;
+
+    // Default constructor for Jackson
+    public GenomesDto() {
+        this.genomes = new ArrayList<>();
+    }
+
+    public GenomesDto(List<GenomeDto> genomes) {
+        this.genomes = genomes;
+    }
+}
